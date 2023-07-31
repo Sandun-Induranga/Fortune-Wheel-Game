e7c586917820c940682bd7b12e3015998dd0ee4d
Sandun Induranga <sandutharu40@gmail.com>
2023-07-31 21:27:53 +0530

    Root Page Created

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_fortune_wheel/flutter_fortune_wheel.dart';
+import 'package:fortune_wheel_game/root_page/root_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,16 +8,16 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurpleAccent),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const RootPage(),
     );
   }
 }
