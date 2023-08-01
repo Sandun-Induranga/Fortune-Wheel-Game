4506582b3e2530cd1891fb7e6e83f7a84e7a6efa
Sandun Induranga <sandutharu40@gmail.com>
2023-08-01 15:09:49 +0530

    Code Refactored

--- lib/main.dart
+++ lib/main.dart
@@ -2,24 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:fortune_wheel_game/ui/root_page/root_page.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.blue,
-        ),
-        useMaterial3: true,
-      ),
-      home: const RootPage(),
-    );
-  }
+  runApp(
+    const FortuneWheelApp(),
+  );
 }
--- lib/ui/root_page/root_page.dart
+++ lib/ui/root_page/root_page.dart
@@ -1,11 +1,21 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:fortune_wheel_game/ui/home_page/home_page.dart';
 
-class RootPage extends StatelessWidget {
-  const RootPage({super.key});
+class FortuneWheelApp extends StatelessWidget {
+  const FortuneWheelApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const HomePage();
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.blue,
+        ),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
   }
 }
